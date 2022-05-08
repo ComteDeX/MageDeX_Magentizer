4aedfa694779c643c290a8750e3fe3e7c87ff07a
Comte de X <comtedex@gmail.com>
2022-05-08 22:07:02 +0200

    refactor and added success message

--- Magentizer/Console/Command/Create/CreateConfigModel.php
+++ Magentizer/Console/Command/Create/CreateConfigModel.php
@@ -116,13 +116,13 @@ class CreateConfigModel extends Command
      * @param string $vendorName
      * @param string $moduleName
      * @return bool
+     * @throws \Magento\Framework\Exception\FileSystemException
      */
     private function createModule(
         string $vendorName,
         string $moduleName,
         OutputInterface $output
     ) : bool {
-
         $vendorPath = $this->rootPath . '/app/code/' . $vendorName;
         $systemXMLFullPath = implode('/', [
             $vendorPath,
@@ -149,40 +149,27 @@ class CreateConfigModel extends Command
         ]);
 
         if($this->driver->isExists($configClassFullPath)) {
-            echo SharedConstants::RED . $configClassPath. ".php file already exists. Exiting without writing anything." . SharedConstants::COLOR_NONE ."\n";
+            echo SharedConstants::RED . $configClassPath .
+                ".php file already exists. Exiting without writing anything." .
+                SharedConstants::NEW_LINE_NO_STYLE;
             return false;
         }
 
         $systemXML = json_decode(json_encode(simplexml_load_string($this->driver->fileGetContents($systemXMLFullPath))), true);
 
-        $configPaths = [];
-            foreach ($systemXML['system']['section'] as $sectionKey => $section) {
-                if ($sectionKey !== "group") { continue; }
-                foreach ($section as $groupsKey => $groups) {
-                    unset($groups['label']);
-                    foreach ($groups as $groupKey => $group) {
-                        if ($groupKey !== "field") { continue; }
-                        foreach ($group as $fieldKey => $field) {
-                            $key = isset($field["config_path"]) ? $field["config_path"] : implode('/',[
-                                $systemXML['system']['section']["@attributes"]['id'],
-                                $systemXML['system']['section']['group'][$groupsKey]["@attributes"]['id'],
-                                $field["@attributes"]['id']
-                            ]);
+        $configPaths = $this->getConfigPaths($systemXML);
 
-                            // 1 for bool, 0 for text and anything else
-                            $configPaths[$key] = (isset($field["source_model"]) && $field["source_model"] === 'Magento\Config\Model\Config\Source\Yesno') ? 1 : 0;
-                        }
-                    }
-                }
-            }
+        if (!count($configPaths)) {
+            echo "File does not seems to be correct. Exiting without writing anything.\n";
+            return false;
+        }
 
-            if (!count($configPaths)) {
-                echo "File does not seems to be correct.\n";
-                return false;
-            }
-
-            $this->createConfigClass($configPaths, $vendorPath, $vendorName, $moduleName);
-
+        if (!$this->createConfigClass($configPaths, $vendorPath, $vendorName, $moduleName)) {
+            echo "Something went wrong. Exiting without writing anything.\n";
+            return false;
+        }
+        echo SharedConstants::GREEN . "Config file ". $configClassPath ." has been correctly created."
+            . sharedConstants::NEW_LINE_NO_STYLE;
         return true;
     }
 
@@ -257,6 +244,55 @@ class CreateConfigModel extends Command
         string $stringToCamelCase
     ) : string {
         return str_replace(' ', '', ucwords(str_replace('_', ' ', $stringToCamelCase)));
+    }
+
+    /**
+     * @param string $systemXMLFullPath
+     * @return array
+     * @throws \JsonException
+     * @throws \Magento\Framework\Exception\FileSystemException
+     */
+    private function getSystemXMLData(string $systemXMLFullPath): array
+    {
+        return json_decode(
+            json_encode(
+                simplexml_load_string(
+                    $this->driver->fileGetContents($systemXMLFullPath)
+                )
+            ), true
+        );
+    }
+
+    /**
+     * @param $systemXML
+     * @return array
+     */
+    private function getConfigPaths($systemXML): array
+    {
+        $configPaths = [];
+        if (!isset($systemXML['system']['section'])) { return $configPaths; }
+
+        foreach ($systemXML['system']['section'] as $sectionKey => $section) {
+            if ($sectionKey !== "group") { continue; }
+            foreach ($section as $groupsKey => $groups) {
+                unset($groups['label']);
+                foreach ($groups as $groupKey => $group) {
+                    if ($groupKey !== "field") { continue; }
+                    foreach ($group as $fieldKey => $field) {
+                        $key = isset($field["config_path"]) ? $field["config_path"] : implode('/',[
+                            $systemXML['system']['section']["@attributes"]['id'],
+                            $systemXML['system']['section']['group'][$groupsKey]["@attributes"]['id'],
+                            $field["@attributes"]['id']
+                        ]);
+
+                        // 1 for bool, 0 for text and anything else
+                        $configPaths[$key] = (isset($field["source_model"]) && $field["source_model"] === 'Magento\Config\Model\Config\Source\Yesno') ? 1 : 0;
+                    }
+                }
+            }
+        }
+
+        return $configPaths;
     }
 }
 
